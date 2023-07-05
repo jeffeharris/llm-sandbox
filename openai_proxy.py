13a9783d39688fcb62ba356914bb4dfda8c614c1
Jeff Harris <jeff.harris6@gmail.com>
2023-07-05 04:20:56 -0400

    clean up a few things in the proxy and html files

--- openai_proxy.py
+++ openai_proxy.py
@@ -15,18 +15,18 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/api', methods=['POST'])
 def api():
     headers = {
         'Authorization': f'Bearer {openai_api_key}',
         'Content-Type': 'application/json'
-    }
-    
+    }    
     
     response = requests.post('https://api.openai.com/v1/chat/completions', headers=headers, json=request.get_json())
-    #response = requests.post('http://localhost:5000/api', 'Content-Type': 'application/json', json=request.get_json())
     
     return jsonify(response.json()), response.status_code
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
